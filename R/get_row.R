965223d290114374044cec4f4698f9bd1d4cff35
Will Mackey <wfmackey@gmail.com>
2019-03-14 21:18:18 +1100

    Create package

---
+++ R/get_row.R
@@ -0,0 +1,117 @@
+#' Work out a particular row of a metrics table.
+#' @name get_row
+#' @param data The data that contains the row to convert.
+#' @param row The row of the table to be analysed.
+#' @param skip Should a particular row be skipped? If so, enter the number or vector of numbers.
+#'
+#' @import dplyr
+#'
+#' @export
+
+
+get_row <- function(row, data, skip = 0) {
+
+  # Set reverse option:
+  reverse <- FALSE
+  rev_char <- as.character(data[row, ncol(data)])
+  if(grepl("(W|w)ors", rev_char)) reverse <- TRUE
+
+  # The end of the line will be "\\ \whitemid" unless it is the last in a chapter's metrics
+  endLine <- "\\\\ \\whitemid"
+
+  # Get the chapter name
+  chap <- as.character(data[row, 1])
+
+  if (!is.na(data[row+1, 1])) endLine <- "\\\\  \\blackmid"
+
+  # If the chapter is NA, set to empty and move on
+  if ( is.na(chap)) {
+    chap <- "                                        & " } else {
+      # If the chapter is not NA:
+      # Count how many metrics the chapter has
+      if (!is.na(data[row+1, 1])) {
+        chaprows <- 1} else if (!is.na(data[row+2, 1])) {
+          chaprows <- 2} else if (!is.na(data[row+3, 1])) {
+            chaprows <- 3} else if (!is.na(data[row+4, 1])) {
+              chaprows <- 4} else if (!is.na(data[row+5, 1])) {
+                chaprows <- 5} else if (!is.na(data[row+6, 1])) {
+                  chaprows <- 6} else {chaprows <- 0}
+
+
+
+      chap <- paste0("\\multirow{", chaprows, "}{=}{\\textbf{", chap, "}}  & ")
+
+
+    }
+
+  # Get the metric name
+  metric <- as.character(data[row, 2])
+  # Fix up latex escape characters
+  metric <- gsub("(%|\\$)", "\\\\\\1", metric, perl = T)
+
+  # Set colours for the row
+
+  # First, set colour codes:
+  # (initially reversed to make "a" mean the highest number)
+  codes <- rev(letters[1:11])
+
+  # And reverse if required (not active yet)
+  if (reverse) codes <- rev(codes)
+
+  # Take a row:
+  a <- data %>%
+    select(-1, -2) %>%
+    slice(row) %>%
+    unlist(., use.names=FALSE)
+
+  suppressWarnings(
+    if (class(a) == "character") a <- as.numeric(a)
+  )
+
+
+  # Get median and sd
+  med <- stats::median(a, na.rm = T)
+  sd <- stats::sd(a, na.rm = T)
+
+  # Create empty object to fill
+  b <- NULL
+
+  set_colours <- function(x) {
+
+    if (!is.na(a[x])) {
+      col <- codes[1]
+      if(a[x] >= med - 1.5*sd) col <- codes[2]
+      if(a[x] >= med - 1.0*sd) col <- codes[3]
+      if(a[x] >= med - 0.5*sd) col <- codes[4]
+      if(a[x] >= med - 0.3*sd) col <- codes[5]
+      if(a[x] >= med)          col <- codes[6]
+      if(a[x] >= med + 0.3*sd) col <- codes[7]
+      if(a[x] >= med + 0.5*sd) col <- codes[8]
+      if(a[x] >= med + 1.0*sd) col <- codes[9]
+      if(a[x] >= med + 1.5*sd) col <- codes[10]
+      if(a[x] >= med + 2.0*sd) col <- codes[11]
+
+      col <- paste0("  & \\q", col, "  " , a[x], "  ")
+    } else {
+      col <- "  &  \\qz  "
+    }
+    print(paste0(a[x], " is ", col))
+    return(col)
+  }
+
+  # Run on row
+  line <- purrr::map(1:length(a), set_colours)
+
+  # Collapse into single vector
+  line <- paste0(line, collapse = "")
+
+  # Add end of line \\
+  line <- paste0(line, endLine)
+
+  # Add chapter name and metrics rows
+  line <- paste0(chap, metric, line, collapse = "")
+
+
+  return(line)
+
+}
---
+++ R/make_metrics_table.R
@@ -0,0 +1,31 @@
+#' Make a metrics table for Grattan
+#' @name make_metrics_table
+#' @param data The data you want to convert into a metrics table.
+#' Note that it should be in the format of a metrics table.
+#'
+#' @import dplyr
+#'
+#' @export
+
+
+## quiets concerns of R CMD check re undefined vars
+globalVariables(c("Australia", "metrics", "."))
+
+
+make_metrics_table <- function(data) {
+
+  # Remove rows that don't have an Australia value (ie gap rows)
+  data <- dplyr::filter(data, !is.na(Australia))
+
+  # Run on all valid rows in the data
+  lines <- data %>%
+    purrr::map(1:nrow(.), get_row, data = .) %>%
+    unlist(lines)
+
+  readr::write_lines(lines, "metrics_table.tex")
+  message(paste0("Yay! The table has been produced and exported as metrics_table.tex",
+                 "\n",
+                 "(Make sure to give it a good once-over, though)"))
+}
+
+
